0ecfe4dafeddcc5234478285da4c40d92268e562
Parker Swinton <parkerdswinton@gmail.com>
2023-10-26 20:52:52 -0700

    Primitive search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,28 @@
 'use client'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY
 
 export default function Home() {
   const [books, setBooks] = useState<any[]>()
-  useEffect(() => {
-    const fetchBook = async () => {
-      try {
-        const res = await fetch(
-          `https://www.googleapis.com/books/v1/volumes?q=intitle:golden+intitle:son&key=${process.env.NEXT_PUBLIC_API_KEY}`
-        )
-        const data = await res.json()
-        console.log(data)
-        setBooks(data.items)
-      } catch (e) {
-        console.log(e)
-      }
+  const [input, setInput] = useState('')
+
+  const handleClick = async () => {
+    try{
+      const res = await axios.get('https://www.googleapis.com/books/v1/volumes', {params: {q: input, key: apiKey}})
+      console.log(res)
+      setBooks(res.data.items)
+      setInput('')
+    } catch (e) {
+      console.log(e)
     }
-    fetchBook()
-  }, [])
+  }
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Bookshelf</h1>
+      <input value={input} onChange={(e) => setInput(e.target.value)} placeholder='Search...'></input>
+      <button onClick={handleClick} disabled={!input}>Search</button>
       {books &&
         books.map((book, idx) => {
           if (book.volumeInfo?.imageLinks?.smallThumbnail)
