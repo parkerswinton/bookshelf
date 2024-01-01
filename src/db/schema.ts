53cd31781782fdb46aac72a984c7274e8eb25739
Parker Swinton <parkerdswinton@gmail.com>
2023-12-31 18:39:59 -0800

    schema: the prequel

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,17 @@
-import { decimal, mysqlTable, serial, smallint, varchar } from "drizzle-orm/mysql-core";
+import {
+  bigint,
+  date,
+  decimal,
+  mysqlTable,
+  serial,
+  smallint,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+export const user = mysqlTable("user", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }),
+});
 
 export const book = mysqlTable("book", {
   id: serial("id").primaryKey(),
@@ -8,4 +21,20 @@ export const book = mysqlTable("book", {
   pageCount: smallint("page_count"),
   averageRating: decimal("average_rating", { precision: 2, scale: 1 }),
   imageLink: varchar("imageLink", { length: 355 }),
+});
+
+export const bookshelf = mysqlTable("bookshelf", {
+  id: serial("id").primaryKey(),
+  userId: bigint("userId", { mode: "number", unsigned: true }).references(() => user.id),
+  name: varchar("name", { length: 100 }),
+});
+
+export const userBook = mysqlTable("user_book", {
+  id: serial("id").primaryKey(),
+  userId: bigint("userId", { mode: "number", unsigned: true }).references(() => user.id),
+  bookId: bigint("bookId", { mode: "number", unsigned: true }).references(() => book.id),
+  bookshelfId: bigint("bookshelfId", { mode: "number", unsigned: true }).references(
+    () => bookshelf.id,
+  ),
+  completeDate: date("completion_date"),
 });
